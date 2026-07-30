cd477a534c68c7785157a6b849a2040bc45d056d
led-un <led-un@users.noreply.github.com>
2026-07-30 15:13:10 +0800

    Add source for 1.21.4 (from verified flightmod-fabric-1.21.6 template)

---
+++ src/main/java/com/github/flightmod/mixin/ServerboundMovePlayerPacketAccessor.java
@@ -0,0 +1,22 @@
+/*
+ * Decompiled with CFR 0.152.
+ * 
+ * Could not load the following classes:
+ *  net.minecraft.class_2828
+ *  org.spongepowered.asm.mixin.Mixin
+ *  org.spongepowered.asm.mixin.Mutable
+ *  org.spongepowered.asm.mixin.gen.Accessor
+ */
+package com.github.flightmod.mixin;
+
+import net.minecraft.class_2828;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(value={class_2828.class})
+public interface ServerboundMovePlayerPacketAccessor {
+    @Mutable
+    @Accessor(value="field_29179")
+    public void setOnGround(boolean var1);
+}
